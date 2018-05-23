5f87e0c88139a6ccc31b222a3d65e7a548b34fba
Grant Foster <grantfoster@Grants-MacBook-Pro.local>
2018-05-23 14:49:36 -0400

    ugh

---
+++ wp-content/themes/goldie/footer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<div class="container">
+			<nav id="site-navigation" class="footer-navigation">
+					<div class="container">
+						<div class='col-12'>
+						<p><?php
+							wp_nav_menu( array(
+								'theme_location' => 'menu-2',
+								'menu_id'        => 'secondary-menu',
+							) );
+							?></p>
+							
+						</div>
+					</div>
+				</nav><!-- #site-navigation -->
+				<p class='small'><i class="fa fa-mobile-alt"></i> 919-249-6730</p>
+				<p class="small">Copyright H.M Stanley Group 2018</p>
+			</div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/goldie/page-jobs.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package _s
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="description-area">
+		<main id="main" class="site-main">
+            <div class='container skinny space'>
+                <div class="row">
+                    <div class='col-12'>
+                        
+
+                        <?php if( have_rows('jobs') ): ?>
+
+                            <?php while( have_rows('jobs') ): the_row(); 
+
+                                // vars
+                                $title = get_sub_field('title');
+                                $description = get_sub_field('description');
+
+                            ?>
+                                <hr>
+                                <h4><?php echo $title ?></h4>
+
+                                <p><?php echo $description; ?></p>
+
+                                <p><a class='gold-cta' href='/apply/?position=<?php echo $title ?>'>Apply</a></p>
+
+                            <?php endwhile; ?>
+
+                        <?php endif; ?>
+
+
+                    </div>
+                </div>	
+            </div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
